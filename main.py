f793aa4c30884a2d74366c8e220836d07286f2e0
bodia07 <bohdan.stefanyk07@gmail.com>
2025-06-11 14:58:02 +0200

    готово

--- main.py
+++ main.py
@@ -6,41 +6,34 @@ init()
 font.init()
 mixer.init()
 
-
 frame_size_x = 720
 frame_size_y = 480
-
 
 LIGHTGREEN = (105, 204, 75)
 BLACK = (0, 0, 0)
 RED = (200, 0, 0)
 WHITE = (255, 255, 255)
 
-
 block_size = 20
-
 
 FONTNAME = "CoralPixels-Regular.ttf"
 score_font = font.Font(FONTNAME, 30)
 
-# Вікно гри
 display.set_caption('Snake Eater')
 clock = time.Clock()
 game_window = display.set_mode((frame_size_x, frame_size_y))
 
 def savescore(score):
     with open('score.json', 'w', encoding='utf-8') as f:
-            json.dump({"score":score}, f,ensure_ascii=False)
+        json.dump({"score":score}, f, ensure_ascii=False)
 
 def loadscore():
     try:
         with open('score.json', 'r', encoding='utf-8') as f:
-            score=json.load(f)
+            score = json.load(f)
             return score["score"]
-    except (FileNotFoundError,json.decoder.JSONDecodeError): 
-        print("файл не знайдено")
+    except (FileNotFoundError, json.decoder.JSONDecodeError): 
         return 0
-
 
 class Label(sprite.Sprite):
     def __init__(self, text, x, y, fontsize=30, color=(225, 228, 232), font_name=FONTNAME):
@@ -94,34 +87,56 @@ class Snake:
         if head in self.body[1:]:
             return True
         return False
-    
-    #def check_collision_food(self):
 
-    
     def draw(self, surface):
         for part in self.body:
             draw.rect(surface, BLACK, Rect(part[0], part[1], block_size, block_size))
 
 class Food():
     def __init__(self):
-        self.position=[random.randrange(0,frame_size_x,block_size),random.randrange(0,frame_size_y,block_size)]
+        self.position = [random.randrange(0, frame_size_x, block_size), random.randrange(0, frame_size_y, block_size)]
 
     def respawn(self):
-        self.position=[random.randrange(0,frame_size_x,block_size),random.randrange(0,frame_size_y,block_size)]
+        self.position = [random.randrange(0, frame_size_x, block_size), random.randrange(0, frame_size_y, block_size)]
 
-    def draw(self,surface):
-        draw.rect(surface,RED,Rect(self.position[0], self.position[1], block_size, block_size))
+    def draw(self, surface):
+        draw.rect(surface, RED, Rect(self.position[0], self.position[1], block_size, block_size))
 
-        
-    
+def start_screen():
+    waiting = True
+    while waiting:
+        game_window.fill(BLACK)
+        title = Label("pres enter to start", 180, 200, 32, WHITE)
+        game_window.blit(title.image, title.rect)
+        display.update()
+        for e in event.get():
+            if e.type == QUIT:
+                quit()
+            elif e.type == KEYDOWN:
+                if e.key == K_RETURN:
+                    waiting = False
+
+
+def game_over_screen(score, maxscore):
+    game_window.fill(BLACK)
+    gameover = Label("Game end", 280, 150, 40, RED)
+    score_l = Label(f"points: {score}", 280, 220, 30, WHITE)
+    maxscore_l = Label(f"max-points: {maxscore}", 280, 260, 30, WHITE)
+    game_window.blit(gameover.image, gameover.rect)
+    game_window.blit(score_l.image, score_l.rect)
+    game_window.blit(maxscore_l.image, maxscore_l.rect)
+    display.update()
+    time.wait(3000)
+
+
+start_screen()
 
 snake = Snake()
-food=Food()
-score=0
-maxscore=loadscore()
-score_lable= Label(f'score:{score}',10,10)
-maxscore_lable=Label(f'maxscore:{maxscore}',500,10)
-
+food = Food()
+score = 0
+maxscore = loadscore()
+score_lable = Label(f'score:{score}', 10, 10)
+maxscore_lable = Label(f'maxscore:{maxscore}', 500, 10)
 
 run = True
 while run:
@@ -137,23 +152,19 @@ while run:
                 snake.change_direction('LEFT')
             elif e.key == K_RIGHT:
                 snake.change_direction('RIGHT')
-    
 
     if snake.move(food.position):
-        score+=1
+        score += 1
         score_lable.set_text(f'score:{score}')
         food.respawn()
-        if score>maxscore:
-            maxscore=score
-            maxscore_lable.set_text(f'Maxsscore:{maxscore}')
+        if score > maxscore:
+            maxscore = score
+            maxscore_lable.set_text(f'maxscore:{maxscore}')
             savescore(maxscore)
-
-
 
     if snake.check_collision():
         run = False
-
-    
+        game_over_screen(score, maxscore)  
 
     game_window.fill(LIGHTGREEN)
     snake.draw(game_window)
@@ -162,4 +173,3 @@ while run:
     game_window.blit(maxscore_lable.image, maxscore_lable.rect)
     display.update()
     clock.tick(10)
-
